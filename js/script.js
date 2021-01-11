c1534492a0e503a90b244341c5686729ed4fb760
Ange Uwimana <uuwange@gmail.com>
2021-01-11 21:55:45 +0200

    Created a working website

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// function myFunction(){
+//     var text;
+//     if (confirm ("We have received your message. Thank you for reaching out to us!")){
+//         text="thank you";
+//     }
+//     document.getElementById("demo").innerHTML = text;
+// }
