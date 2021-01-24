39da0a3d1f30b09414934a3e1bcbf47fe10bedf0
Ange Uwimana <uuwange@gmail.com>
2021-01-24 19:19:37 +0200

    Fixed errors

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@
 //     }
 //     document.getElementById("demo").innerHTML = text;
 // }
+
+
+<script>
+
+var frmvalidator = new Validator(“contactform”); 
+frmvalidator.addValidation(“name”,”req”,”Please provide your name”); 
+frmvalidator.addValidation(“email”,”req”,”Please provide your email”); 
+frmvalidator.addValidation(“email”,”email”, “Please enter a valid email address”); 
+</script>
